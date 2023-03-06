edd879f790011f407f599c83ced8ae3f12a99016
Diego Dominguez <dominguezdiegotristan@gmail.com>
2023-03-06 16:16:18 -0600

    Instalación de jetstream

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,13 @@ Route::get('/precios/{cupon_id?}', [PaginaController::class, 'precios']);
 
 Route::resource('pintura', PinturaController::class);
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
